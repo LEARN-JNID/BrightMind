62cb668f77753e1008dda99059673eec426f69cb
ios10 <trikshotzz05@gmail.com>
2022-07-13 11:59:14 -0700

    protected index

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -13,13 +13,34 @@ import PostEdit from './pages/PostEdit'
 import Footer from './components/Footer'
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    this.readPost()
+  }
+  readPost = () => {
+    fetch('/posts')
+      .then(res => res.json())
+      .then(payload => this.setState({ posts: payload }))
+      .catch(errors => console.log(errors))
+  }
+
   render () {
+    const {current_user} = this.props
     return (
     <Router>
       <Header {...this.props} />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/postindex" component={PostIndex} />
+          <Route path="/postindex" render={() => {
+              let myPost = this.state.posts.filter(post => post.user_id === current_user.id)
+              return < PostIndex posts = { myPost } />
+            }} />
           <Route path="/postnew" component={PostNew} />
           <Route path="/postshow/:id" component={PostShow} />
           <Route path="/postedit" component={PostEdit} />
