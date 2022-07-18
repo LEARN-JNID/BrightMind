c183fb7317a558543d14db3d99134bcaec3c5431
ios10 <trikshotzz05@gmail.com>
2022-07-18 14:08:32 -0700

    filter

--- app/javascript/components/pages/PostIndex.js
+++ app/javascript/components/pages/PostIndex.js
@@ -9,14 +9,44 @@ import {
   CardSubtitle, 
   Button
 } from 'reactstrap';
+// import Filter from '../components/Filter';
 
 export default class PostIndex extends Component {
+
+
+  filterChoice(e){
+    if (e.target.value !== "Date"){
+      let filteredPost = this.state.posts.filter(post => {
+        return post.mood == e.target.value
+      })
+    }
+    this.setState({filterChoice: e.target.value})
+  }
+
+
+// filterChoice(e){
+//   this.setState({
+//     posts: this.state.post.filter(function(choice) {
+//       return choice == e.target.value
+//     })
+//   })
+// }
+
+  
   render() {
     const {posts} = this.props
     return (
       <>
         <h3>My Journal's</h3>
-        <div>
+        <select onChange={this.filterChoice}>
+          <option value="Happy">Happy</option>
+          <option value="Good">Good</option>
+          <option value="Okay">Okay</option>
+          <option value="Sad">Sad</option>
+          <option value="Miserable">Miserable</option>
+          <option value="Date">Date Time</option>
+        </select>
+        <div value={this.state.filterChoice}>
           {posts &&
            posts.map((currentPost, index)=> {
             return ( 
