b3bfc0a6bbaaf6c327eaa99e30529dc28d43093a
Nic Last <niclast7611@gmail.com>
2022-07-19 10:28:06 -0700

    styled show page and added modal to the delete button

--- app/javascript/components/pages/PostShow.js
+++ app/javascript/components/pages/PostShow.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react'
 import { NavLink, Redirect } from 'react-router-dom'
+import {GoPrimitiveDot} from 'react-icons/go'
+import {BsCalendar, BsClock} from 'react-icons/bs'
+import {
+  Card, 
+  CardText, 
+  CardBody,
+  CardTitle, 
+  CardSubtitle, 
+  Button,
+  Modal,
+  ModalHeader,
+  ModalFooter
+} from 'reactstrap';
 
 export default class PostShow extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        deleted: false
+        deleted: false,
+        modal: false
     }
+    this.toggleModal = this.toggle.bind(this);
   }
 
   handleDelete = () => {
         this.props.deletePost(this.props.post.id)
         this.setState({deleted: true})
+    }
+
+    toggle = () =>  {
+      this.setState({
+        modal: !this.state.modal
+      });
     }
 
   render() {
@@ -20,23 +41,43 @@ export default class PostShow extends Component {
     }
     console.log("THIS IS PROPS: ", this.props)
     return (
-      <>
+      <div id='show-body'>
+         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Are you sure you want to delete this journal?</ModalHeader>
+          <ModalFooter>
+            <Button color="primary" onClick={this.handleDelete}>Forget Journal</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
     {this.props.logged_in &&
-    <div className='show_post'> 
-      <h3>PostShow</h3>
-      <h1>{this.props.post.title}</h1>
-      <h6>{this.props.post.created_at}</h6>
-      <h6>{this.props.post.mood}</h6>
-      <p>{this.props.post.body}</p>
-      <NavLink to={`/postedit/${this.props.post.id}`}>
-          Edit Entry
-      </NavLink>
-      <button onClick={this.handleDelete}>
-          Delete Entry
-      </button>
-      </div>
+    <Card 
+    id='show-card'
+    style={{ width: '100%', height: 'fit-content'}}
+    >
+      <CardBody>
+        <CardTitle id='show-title'>{this.props.post.title}</CardTitle>
+        <CardSubtitle className='show-sub'>
+        <BsCalendar id='show-icons'/>{` ${this.props.post.created_at.slice(0, 10)}`}</CardSubtitle>
+        <CardSubtitle className='show-sub'>
+        <BsClock id='show-icons'/>{` ${this.props.post.created_at.slice(11, 16)}`}</CardSubtitle>
+        <div id='mood-div-c'>
+          <CardSubtitle id='show-mood'>{this.props.post.mood}</CardSubtitle>
+          <GoPrimitiveDot id='show-mood'/>
+        </div>
+        <CardText className='show-text'>{this.props.post.body}</CardText>
+        <Button className='show-buttons'>
+          <NavLink id='navlink-show' to={'/postindex'}> Go Back </NavLink>
+        </Button>
+        <Button className='show-buttons'>
+          <NavLink id='navlink-show' to={`/postedit/${this.props.post.id}`}> Edit Entry </NavLink>
+        </Button>
+        <Button className='show-buttons' onClick={this.toggle}>
+           Forget Entry
+        </Button>
+      </CardBody>
+    </Card>
     }
-      </>
+      </div>
     )
   }
 }
