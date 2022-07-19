04f0f9f144f2f0be77c372d4fdf412354c4712cd
Nic Last <niclast7611@gmail.com>
2022-07-19 10:40:29 -0700

    fixed indentation and added prettier code formatter

--- app/javascript/components/pages/PostShow.js
+++ app/javascript/components/pages/PostShow.js
@@ -29,55 +29,56 @@ export default class PostShow extends Component {
         this.setState({deleted: true})
     }
 
-    toggle = () =>  {
-      this.setState({
-        modal: !this.state.modal
-      });
-    }
+  toggle = () =>  {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
 
-  render() {
-    if(this.state.deleted) {
-      return(<Redirect to="/postindex" />)
-    }
-    console.log("THIS IS PROPS: ", this.props)
-    return (
-      <div id='show-body'>
-         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}>Are you sure you want to delete this journal?</ModalHeader>
-          <ModalFooter>
-            <Button color="primary" onClick={this.handleDelete}>Forget Journal</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-    {this.props.logged_in &&
-    <Card 
-    id='show-card'
-    style={{ width: '100%', height: 'fit-content'}}
-    >
-      <CardBody>
-        <CardTitle id='show-title'>{this.props.post.title}</CardTitle>
-        <CardSubtitle className='show-sub'>
-        <BsCalendar id='show-icons'/>{` ${this.props.post.created_at.slice(0, 10)}`}</CardSubtitle>
-        <CardSubtitle className='show-sub'>
-        <BsClock id='show-icons'/>{` ${this.props.post.created_at.slice(11, 16)}`}</CardSubtitle>
-        <div id='mood-div-c'>
-          <CardSubtitle id='show-mood'>{this.props.post.mood}</CardSubtitle>
-          <GoPrimitiveDot id='show-mood'/>
-        </div>
-        <CardText className='show-text'>{this.props.post.body}</CardText>
-        <Button className='show-buttons'>
-          <NavLink id='navlink-show' to={'/postindex'}> Go Back </NavLink>
-        </Button>
-        <Button className='show-buttons'>
-          <NavLink id='navlink-show' to={`/postedit/${this.props.post.id}`}> Edit Entry </NavLink>
-        </Button>
-        <Button className='show-buttons' onClick={this.toggle}>
-           Forget Entry
-        </Button>
-      </CardBody>
-    </Card>
-    }
-      </div>
+render() {
+  if(this.state.deleted) {
+    return(<Redirect to="/postindex" />)
+  }
+  console.log("THIS IS PROPS: ", this.props)
+  return (
+    <>
+      <Modal isOpen={this.state.modal} toggle={this.toggle}>
+        <ModalHeader toggle={this.toggle}>Are you sure you want to delete this journal?</ModalHeader>
+        <ModalFooter>
+          <Button color="primary" onClick={this.handleDelete}>Forget Journal</Button>{' '}
+          <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+        </ModalFooter>
+    </Modal>
+    <div id='show-body'>   
+      {this.props.logged_in &&
+      <Card 
+      id='show-card'
+      style={{ width: '100%', height: 'fit-content'}}>
+        <CardBody>
+          <CardTitle id='show-title'>{this.props.post.title}</CardTitle>
+          <CardSubtitle className='show-sub'>
+          <BsCalendar id='show-icons'/>{` ${this.props.post.created_at.slice(0, 10)}`}</CardSubtitle>
+          <CardSubtitle className='show-sub'>
+          <BsClock id='show-icons'/>{` ${this.props.post.created_at.slice(11, 16)}`}</CardSubtitle>
+          <div id='mood-div-c'>
+            <CardSubtitle id='show-mood'>{this.props.post.mood}</CardSubtitle>
+            <GoPrimitiveDot id='show-mood'/>
+          </div>
+          <CardText className='show-text'>{this.props.post.body}</CardText>
+          <Button className='show-buttons'>
+            <NavLink id='navlink-show' to={'/postindex'}> Go Back </NavLink>
+          </Button>
+          <Button className='show-buttons'>
+            <NavLink id='navlink-show' to={`/postedit/${this.props.post.id}`}> Edit Entry </NavLink>
+          </Button>
+          <Button className='show-buttons' onClick={this.toggle}>
+            Forget Entry
+          </Button>
+        </CardBody>
+      </Card>
+      }
+    </div>
+  </>
     )
   }
 }
