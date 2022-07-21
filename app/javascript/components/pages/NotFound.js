0d244da589bd091c0d35364578e7000a2b5f44cf
Nic Last <niclast7611@gmail.com>
2022-07-21 11:21:59 -0700

    not found page complete

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class NotFound extends Component {
   render() {
     return (
-      <h3>NotFound</h3>
+      <div id='nf-container'>
+        <img className='not-img' src="https://cdni.iconscout.com/illustration/free/thumb/relaxing-mind-2080822-1749566.png" alt='Yoga pose inside head'/>
+        <h1 id='nf-h1'>404 not found</h1>
+      </div>
     )
   }
 }
